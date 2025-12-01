8b9ef2d2bc9612206e72da1b7416daf85dce55f5
leeseonju12 <seonju220@gmail.com>
2025-12-01 17:22:31 +0900

    10 * 10 * 10

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -32,12 +32,7 @@ public class Calc {
 
             int a = Integer.parseInt(bits[0]);
             int b = Integer.parseInt(bits[1]);
-            int c = 0;
-
-            if (bits.length > 2) {
-                c = Integer.parseInt(bits[2]);
-            }
-
+            int c = Integer.parseInt(bits[2]);;
             return a * b * c;
         }
         throw new RuntimeException("해석 불가 : 올바른 계산식이 아님");
